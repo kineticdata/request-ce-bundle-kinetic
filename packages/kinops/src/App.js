082fcf03fa1b8882dcce0ae3f4c43c6c230b4ce6
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-02-26 09:07:09 -0600

    hooked up sidebar component and redux state

--- packages/kinops/src/App.js
+++ packages/kinops/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import { compose, lifecycle } from 'recompose';
+import { compose, lifecycle, withHandlers } from 'recompose';
+import Sidebar from 'react-sidebar';
 import { ToastsContainer } from './components/ToastsContainer';
 import { HeaderContainer } from './components/HeaderContainer';
 import { ModalFormContainer } from './components/ModalFormContainer';
 import { actions as kinopsActions } from './redux/modules/kinops';
 import { actions as alertsActions } from './redux/modules/alerts';
+import { actions as layoutActions } from './redux/modules/layout';
 import { AppRouter } from './AppRouter';
 
 import 'bootstrap/scss/bootstrap.scss';
@@ -16,27 +18,39 @@ export const AppComponent = props => (
   <Fragment>
     <ToastsContainer />
     <ModalFormContainer />
-    <HeaderContainer
-      hasSidebar
-      toggleSidebarOpen={() => console.log('toggle')}
-    />
-    <div className="App" style={{ marginTop: '49px' }}>
-      {!props.loading && <AppRouter kapps={props.kapps} />}
-    </div>
+    <HeaderContainer hasSidebar toggleSidebarOpen={props.toggleSidebarOpen} />
+    <Sidebar
+      sidebar={<div style={{ marginTop: '49px' }}>Sidebar Content</div>}
+      shadow={false}
+      open={props.sidebarOpen && props.layoutSize === 'small'}
+      docked={props.sidebarOpen && props.layoutSize !== 'small'}
+      onSetOpen={props.setSidebarOpen}
+      sidebarClassName={`sidebar ${true ? 'drawer' : 'overlay'}`}
+    >
+      <div className="App" style={{ marginTop: '49px' }}>
+        {!props.loading && <AppRouter kapps={props.kapps} />}
+      </div>
+    </Sidebar>
   </Fragment>
 );
 
 export const mapStateToProps = state => ({
   loading: state.loading,
   kapps: state.kinops.kapps,
+  sidebarOpen: state.layout.sidebarOpen,
+  layoutSize: state.layout.size,
 });
 export const mapDispatchToProps = {
   loadApp: kinopsActions.loadApp,
   fetchAlerts: alertsActions.fetchAlerts,
+  setSidebarOpen: layoutActions.setSidebarOpen,
 };
 
 export const App = compose(
   connect(mapStateToProps, mapDispatchToProps),
+  withHandlers({
+    toggleSidebarOpen: props => () => props.setSidebarOpen(!props.sidebarOpen),
+  }),
   lifecycle({
     componentDidMount() {
       this.props.loadApp();
--- packages/kinops/src/redux/modules/layout.js
+++ packages/kinops/src/redux/modules/layout.js
@@ -1,22 +1,60 @@
 import { Record } from 'immutable';
+import { LOCATION_CHANGE } from 'connected-react-router';
 import { namespace, withPayload } from 'common/utils';
 
 export const types = {
   SET_SIZE: namespace('layout', 'SET_SIZE'),
+  SET_SIDEBAR_OPEN: namespace('layout', 'SET_SIDEBAR_OPEN'),
+  SET_LIMITED_SIDEBAR_OPEN: namespace('layout', 'SET_LIMITED_SIDEBAR_OPEN'),
 };
 
 export const actions = {
   setSize: withPayload(types.SET_SIZE),
+  setSidebarOpen: payload => ({ type: types.SET_SIDEBAR_OPEN, payload }),
+  setLimitedSidebarOpen: payload => ({
+    type: types.SET_LIMITED_SIDEBAR_OPEN,
+    payload,
+  }),
 };
 
 export const State = Record({
   size: 'small',
+  sidebarOpen: true,
+  // There are some pages where we want the sidebar to be less of a focus in the
+  // user interface (the home page of the services kapp is one example). The
+  // limitedSidebarOpen state tracks whether or not the sidebar should be open
+  // on these kinds of pages. It defaults to false and needs to be opened
+  // manually, then if the sidebar is closed from another page we assume that
+  // the sidebar on these types of pages should also then be closed (all of that
+  // logic is implemented in the reducer below).
+  limitedSidebarOpen: false,
 });
 
 export const reducer = (state = State(), { type, payload }) => {
   switch (type) {
     case types.SET_SIZE:
       return state.set('size', payload);
+    case types.SET_SIDEBAR_OPEN:
+      return (
+        state
+          .set('sidebarOpen', payload)
+          // if we are closing the sidebar from regular pages we assume that we
+          // should also close the sidebar for the "limited" pages as well.
+          .set(
+            'limitedSidebarOpen',
+            !payload ? payload : state.limitedSidebarOpen,
+          )
+      );
+    case types.SET_LIMITED_SIDEBAR_OPEN:
+      return (
+        state
+          .set('limitedSidebarOpen', payload)
+          // if we are openening the sidebar from the "limited" page we assume
+          // that we should also open the sidebar for the regular pages as well.
+          .set('sidebarOpen', payload ? payload : state.sidebarOpen)
+      );
+    case LOCATION_CHANGE:
+      return state.size === 'small' ? state.set('sidebarOpen', false) : state;
     default:
       return state;
   }
