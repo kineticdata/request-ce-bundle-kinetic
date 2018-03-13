183bd44529bfb5add8836785aaf765006db85472
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-03-13 16:52:00 -0500

    cleared space package directory

--- packages/space/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SpaceHome = () => <div>Space Home Page</div>;
-export const TeamList = () => <div>Team List Page</div>;
-export const Team = () => <div>Team Page</div>;
