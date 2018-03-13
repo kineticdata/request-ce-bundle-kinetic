70f994a4b7e12ff3d3cc4dbbd5d94c1fcd1b12b2
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-03-13 16:36:27 -0500

    removed unused stuff

--- packages/queue/src/components/home/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DocumentTitle from 'react-document-title';
-
-export const Home = () => (
-  <DocumentTitle title="Home">
-    <div>Hello World!</div>
-  </DocumentTitle>
-);
