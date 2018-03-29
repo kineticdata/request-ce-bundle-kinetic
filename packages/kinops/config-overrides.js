ded219bf2656096fd2779ae2eedcb74001540287
Chad Rehm <chad.rehm@kineticdata.com>
2018-03-29 15:55:03 -0500

    fixed windows path issue

--- packages/kinops/config-overrides.js
+++ packages/kinops/config-overrides.js
@@ -1,4 +1,19 @@
-const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
+const getWorkspaces = require('get-yarn-workspaces')
+const path = require('path')
+
+function rewireYarnWorkspaces(config, env, from) {
+  const babel = config.module.rules
+    .find(rule => 'oneOf' in rule)
+    .oneOf.find(rule => /babel-loader/.test(rule.loader))
+    
+    if (!Array.isArray(babel.include)) {
+      babel.include = [babel.include]
+  }
+  
+  babel.include = babel.include.concat(getWorkspaces(from).map((directory) => path.resolve(directory)))
+  
+  return config
+}
 
 module.exports = function override(config, env) {
   return rewireYarnWorkspaces(config, env);
