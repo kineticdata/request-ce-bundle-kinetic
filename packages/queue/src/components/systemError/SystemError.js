96603dc71e63efe90d6a749d2b63abb786210867
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-04-03 13:41:21 -0500

    rearranged queue components to be more consistent with space, removed some unused components

--- packages/queue/src/components/systemError/SystemError.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SystemError = ({ status, statusText }) => (
-  <div>
-    <h1>There was a problem!</h1>
-    <h4>{status}</h4>
-    <p>{statusText || 'Unknown error has occurred.'}</p>
-  </div>
-);
