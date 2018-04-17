87c9adbc12cf89e116a068960d7175f10600ebbc
Matt Raykowski <matt.raykowski@kineticdata.com>
2018-04-17 10:13:02 -0500

    KD-709: Added CE version check to datastore routes.

--- packages/space/src/components/settings/datastore/Datastore.js
+++ packages/space/src/components/settings/datastore/Datastore.js
@@ -8,41 +8,66 @@ import { SubmissionSearch } from './SubmissionSearch/SubmissionSearch';
 import { DatastoreSettings } from './DatastoreSettings';
 import { CreateDatastore } from './CreateDatastore';
 import { actions } from '../../../redux/modules/settingsDatastore';
+import semver from 'semver';
+
+const MINIMUM_CE_VERSION = '2.1.0';
 
 const DatastoreError = () => <h1>Error loading Datastore</h1>;
+const DatastoreVersionError = ({ version }) => (
+  <div className="pane">
+    <h1>Invalid CE Version</h1>
+    <p>
+      {`You are currently running Kinetic CE ${version.version}. Datastore
+      requires Kinetic CE ${MINIMUM_CE_VERSION} or greater.`}
+    </p>
+  </div>
+);
 
-export const DatastoreRouter = ({ match, loading }) =>
-  !loading && (
-    <Switch>
-      <Route exact path={`${match.path}/new`} component={CreateDatastore} />
-      <Route exact path={`${match.path}/error`} component={DatastoreError} />
-      <Route exact path={`${match.path}/:slug`} component={SubmissionSearch} />
-      <Route
-        exact
-        path={`${match.path}/:slug/settings`}
-        component={DatastoreSettings}
-      />
-      <Route
-        exact
-        path={`${match.path}/:slug/new`}
-        component={DatastoreSubmission}
-      />
-      <Route
-        exact
-        path={`${match.path}/:slug/:id`}
-        component={DatastoreSubmission}
-      />
-      <Route
-        exact
-        path={`${match.path}/:slug/:id/:mode`}
-        component={DatastoreSubmission}
-      />
-      <Route component={FormList} />
-    </Switch>
+export const DatastoreRouter = ({ match, loading, validVersion, version }) =>
+  !validVersion ? (
+    <DatastoreVersionError version={version} />
+  ) : (
+    !loading && (
+      <Switch>
+        <Route exact path={`${match.path}/new`} component={CreateDatastore} />
+        <Route exact path={`${match.path}/error`} component={DatastoreError} />
+        <Route
+          exact
+          path={`${match.path}/:slug`}
+          component={SubmissionSearch}
+        />
+        <Route
+          exact
+          path={`${match.path}/:slug/settings`}
+          component={DatastoreSettings}
+        />
+        <Route
+          exact
+          path={`${match.path}/:slug/new`}
+          component={DatastoreSubmission}
+        />
+        <Route
+          exact
+          path={`${match.path}/:slug/:id`}
+          component={DatastoreSubmission}
+        />
+        <Route
+          exact
+          path={`${match.path}/:slug/:id/:mode`}
+          component={DatastoreSubmission}
+        />
+        <Route component={FormList} />
+      </Switch>
+    )
   );
 
 export const mapStateToProps = state => ({
   loading: state.settingsDatastore.loading,
+  version: state.kinops.version,
+  validVersion: semver.satisfies(
+    semver.coerce(state.kinops.version.version),
+    `>=${MINIMUM_CE_VERSION}`,
+  ),
 });
 
 export const mapDispatchToProps = {
@@ -52,8 +77,10 @@ export const mapDispatchToProps = {
 export const Datastore = compose(
   connect(mapStateToProps, mapDispatchToProps),
   lifecycle({
-    componentWillMount(prev, next) {
-      this.props.fetchForms();
+    componentWillMount() {
+      if (this.props.validVersion) {
+        this.props.fetchForms();
+      }
     },
   }),
 )(DatastoreRouter);
