3e9944bb3cf499fd6f02942978cd027c1a9d89a5
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-05-06 22:15:50 -0500

    added message when notification datastore forms are missing

--- packages/space/src/components/settings/notifications/Notifications.js
+++ packages/space/src/components/settings/notifications/Notifications.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Notification } from './Notification';
 import { DateFormat } from './DateFormat';
+import { NotificationsNotFound } from './NotificationsNotFound';
 import { NotificationsList } from './NotificationsList';
+import {
+  NOTIFICATIONS_FORM_SLUG,
+  NOTIFICATIONS_DATE_FORMAT_FORM_SLUG,
+} from '../../../redux/modules/settingsNotifications';
 
-export const Notifications = ({ match }) => (
-  <Switch>
-    <Route path={`${match.path}/date-formats/:id`} component={DateFormat} />
-    <Route path={`${match.path}/:type/:id`} component={Notification} />
-    <Route path={`${match.path}/:type`} component={NotificationsList} />
-    <Route render={() => <Redirect to={`${match.path}/templates`} />} />
-  </Switch>
-);
+export const NotificationsComponent = ({ match, showNotifications }) =>
+  showNotifications ? (
+    <Switch>
+      <Route path={`${match.path}/date-formats/:id`} component={DateFormat} />
+      <Route path={`${match.path}/:type/:id`} component={Notification} />
+      <Route path={`${match.path}/:type`} component={NotificationsList} />
+      <Route render={() => <Redirect to={`${match.path}/templates`} />} />
+    </Switch>
+  ) : (
+    <NotificationsNotFound />
+  );
+
+export const mapStateToProps = state => ({
+  showNotifications:
+    state.settingsDatastore.forms
+      .map(form => form.slug)
+      .toSet()
+      .intersect([NOTIFICATIONS_FORM_SLUG, NOTIFICATIONS_DATE_FORMAT_FORM_SLUG])
+      .size === 2,
+});
+
+export const Notifications = connect(mapStateToProps)(NotificationsComponent);
