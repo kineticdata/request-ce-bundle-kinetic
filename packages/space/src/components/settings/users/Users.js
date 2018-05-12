ad053423ba07395c2c4feb4816b312ceac21f4c8
Chad Rehm <chad.rehm@kineticdata.com>
2018-05-12 06:43:29 -0500

    KIN-732 Refactoring for edit user mode to allow for shared component use.

--- packages/space/src/components/settings/users/Users.js
+++ packages/space/src/components/settings/users/Users.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { UsersList } from './UsersList';
 import { ViewUser } from './ViewUser';
-import { EditUser } from './EditUser';
+import { UserForm } from './UserForm';
 
 const UsersError = () => <h1>Error loading Datastore</h1>;
 const NewUser = () => <h1>New User</h1>;
@@ -13,7 +13,7 @@ export const Users = ({ match }) => (
     <Route exact path={`${match.path}/new`} component={NewUser} />
     <Route exact path={`${match.path}/import`} component={ImportUsers} />
     <Route exact path={`${match.path}/error`} component={UsersError} />
-    <Route exact path={`${match.path}/:username/edit`} component={EditUser} />
+    <Route exact path={`${match.path}/:username/edit`} component={UserForm} />
     <Route exact path={`${match.path}/:username`} component={ViewUser} />
     <Route component={UsersList} />
   </Switch>
