d22f84af367ff754743f1bfe7f32a7939e6f0d68
Norman Orstad <norm.orstad@kineticdata.com>
2018-05-14 14:32:16 -0500

    KIN-809. Colors for design systems

--- packages/design-system/src/ColorsPage.js
+++ packages/design-system/src/ColorsPage.js
@@ -6,13 +6,289 @@ export const ColorsPage = () => (
       <div className="col-10">
         <h1>Colors</h1>
         <hr />
-        <h2>Structure</h2>
-
-        <div className="flex-row">
-          <div className="highlight">
-            <pre>Code goes here</pre>
+        <h2>Basic</h2>
+        <div className="cards__wrapper cards__wrapper--design-system">
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(16, 148, 196)' }}
+            />
+            <div className="card--color__body">
+              <h2>Blue</h2>
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$blue </dd>
+                <dt>RGB</dt>
+                <dd>rgb(16, 148, 196) </dd>
+                <dt>Hex</dt>
+                <dd>#1094C4 </dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(168, 183, 199)' }}
+            />
+            <div className="card--color__body">
+              <h2>Blue Gray</h2>
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$blue-gray </dd>
+                <dt>RGB</dt>
+                <dd>rgb(168, 183, 199)</dd>
+                <dt>Hex</dt>
+                <dd>#A8B7C7</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(9, 84, 130)' }}
+            />
+            <div className="card--color__body">
+              <h2>$blue-lake</h2>
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$blue-lake </dd>
+                <dt>RGB</dt>
+                <dd>rgb(9, 84, 130)</dd>
+                <dt>HEX</dt>
+                <dd>#095482</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(12, 56, 79)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$blue-slate</dd>
+                <dt>RGB</dt>
+                <dd>rgb(12, 56, 79)</dd>
+                <dt>HEX</dt>
+                <dd>#0C384F</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(11, 168, 224)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$blue-sky</dd>
+                <dt>RGB</dt>
+                <dd>rgb(11, 168, 224)</dd>
+                <dt>HEX</dt>
+                <dd>#0BA8E0</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(0, 212, 106)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$green-grass</dd>
+                <dt>RGB</dt>
+                <dd>rgb(0, 212, 106)</dd>
+                <dt>HEX</dt>
+                <dd>#00D46A</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(102, 225, 65)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$green</dd>>
+                <dt>RGB</dt>
+                <dd>rgb(102, 225, 65)</dd>
+                <dt>HEX</dt>
+                <dd>#66E041</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(2, 212, 177)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$green-teal</dd>
+                <dt>RGB</dt>
+                <dd>rgb(2, 212, 177)</dd>
+                <dt>HEX</dt>
+                <dd>#02D4B1</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(255, 153, 28)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$orange</dd>
+                <dt>RGB</dt>
+                <dd>rgb(255, 153, 28)</dd>
+                <dt>HEX</dt>
+                <dd>#FF991C</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(214, 105, 42)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$orange-kinetic</dd>
+                <dt>RGB</dt>
+                <dd>rgb(214, 105, 42);</dd>
+                <dd>HEX</dd>
+                <dd> #D6692A</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(255, 119, 0)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$orange-kinops</dd>
+                <dt>RGB</dt>
+                <dd>rgb(255, 119, 0)</dd>
+                <dt>HEX</dt>
+                <dd>#FF7700</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(166, 48, 150)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$purple</dd>
+                <dt>RGB</dt>
+                <dd>rgb(166, 48, 150)</dd>
+                <dt> HEX</dt>
+                <dd>#A63096</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(191, 52, 121)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$red-purple</dd>
+                <dt>RGB</dt>
+                <dd>rgb(191, 52, 121)</dd>
+                <dt>HEX</dt>
+                <dd>#BF3479</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(250, 58, 55)' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$red</dd>
+                <dt>RGB</dt>
+                <dd>rgb(250, 58, 55)</dd>
+                <dt>HEX</dt>
+                <dd>#FA3A37</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(255, 74, 94  )' }}
+            />
+            <div className="card--color__body">
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$red-rose</dd>
+                <dt>RGB</dt>
+                <dd>rgb(255, 74, 94)</dd>
+                <dt>HEX</dt>
+                <dd>#FF4A5E</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(254, 233, 78)' }}
+            />
+            <div className="card--color__body">
+              <h2>Yellow</h2>
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$yellow</dd>
+                <dt>RGB</dt>
+                <dd>rgb(254, 233, 78)</dd>
+                <dt>HEX</dt>
+                <dd>#FEE94D</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="card card--color">
+            <div
+              className="card--color__header"
+              style={{ backgroundColor: 'rgb(255, 207, 74)' }}
+            />
+            <div className="card--color__body">
+              <h2>Sunflower</h2>
+              <dl>
+                <dt>SCSS</dt>
+                <dd>$sunflower</dd>
+                <dt>RGB</dt>
+                <dd>rgb(255, 207, 74)</dd>
+                <dt>HEX</dt>
+                <dd>#FFCF4A</dd>
+              </dl>
+            </div>
           </div>
         </div>
+        <hr />
+        <h2>Grays</h2>
+        <hr />
       </div>
     </div>
   </Fragment>
