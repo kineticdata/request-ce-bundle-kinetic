765e2703144dc49694de7eb84333f0dee933be72
Stephanie Harms <sharms2010@desktop01.kineticdata.com>
2018-06-13 15:29:18 -0500

    Sidebar bare bones

--- packages/services/src/components/settings/Sidebar.js
+++ packages/services/src/components/settings/Sidebar.js
@@ -3,35 +3,19 @@ import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose, withProps } from 'recompose';
 import { bundle } from 'react-kinetic-core';
-import { selectHasSharedTaskEngine } from '../../redux/modules/spaceApp';
 
-import { NOTIFICATIONS_FORM_SLUG } from '../../redux/modules/settingsNotifications';
-
-export const SidebarComponent = ({
-  settingsBackPath,
-  hasSharedTaskEngine,
-  loading,
-  spaceAdmin,
-}) => (
+export const SidebarComponent = ({ settingsBackPath, loading, spaceAdmin }) => (
   <div className="sidebar space-sidebar">
-    <Link to={settingsBackPath} className="nav-return">
+    <Link to="/kapps/services" className="nav-return">
       <span className="fa fa-fw fa-chevron-left" />
       Return to Home
     </Link>
     {!loading && (
       <ul className="nav flex-column settings-group">
         <li className="nav-item">
-          <NavLink
-            to="/settings/profile"
-            className="nav-link"
-            activeClassName="active"
-          >
-            Profile
-            <span className="fa fa-fw fa-angle-right" />
-          </NavLink>
           {spaceAdmin && (
             <NavLink
-              to="/settings/space"
+              to="/kapps/services/settings"
               className="nav-link"
               activeClassName="active"
             >
@@ -39,64 +23,17 @@ export const SidebarComponent = ({
               <span className="fa fa-fw fa-angle-right" />
             </NavLink>
           )}
-          {spaceAdmin && (
-            <NavLink
-              to="/settings/users"
-              className="nav-link"
-              activeClassName="active"
-            >
-              Users
-              <span className="fa fa-fw fa-angle-right" />
-            </NavLink>
-          )}
         </li>
       </ul>
-    )}
-    {spaceAdmin && (
-      <div className="sidebar-group sidebar-group--settings">
-        <ul className="nav flex-column settings-group">
-          <li>
-            <a
-              href={`${bundle.spaceLocation()}/app`}
-              target="blank"
-              className="nav-link"
-            >
-              Kinetic Request Admin
-              <span className="fa fa-fw fa-external-link" />
-            </a>
-          </li>
-          {!hasSharedTaskEngine && (
-            <li>
-              <a
-                href={`${bundle.spaceLocation()}/kinetic-task`}
-                target="blank"
-                className="nav-link"
-              >
-                Kinetic Task Admin
-                <span className="fa fa-fw fa-external-link" />
-              </a>
-            </li>
-          )}
-        </ul>
-      </div>
     )}
   </div>
 );
 
 export const mapStateToProps = state => ({
-  loading: state.space.settingsDatastore.loading,
-  forms: state.space.settingsDatastore.forms,
+  loading: state.services.servicesSettings.loading,
+  forms: state.services.forms.data,
   spaceAdmin: state.app.profile.spaceAdmin,
   pathname: state.router.location.pathname,
-  hasSharedTaskEngine: selectHasSharedTaskEngine(state),
 });
 
-export const Sidebar = compose(
-  connect(mapStateToProps),
-  withProps(props => ({
-    showDatastore: props.spaceAdmin || !props.forms.isEmpty(),
-    showNotifications: !!props.forms.find(
-      form => form.slug === NOTIFICATIONS_FORM_SLUG,
-    ),
-  })),
-)(SidebarComponent);
+export const Sidebar = compose(connect(mapStateToProps))(SidebarComponent);
