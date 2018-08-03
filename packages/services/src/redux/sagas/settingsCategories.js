4830fc9c6a59cbb133f480635b3de0fe97aefa9f
Stephanie Harms <sharms2010@desktop01.kineticdata.com>
2018-08-03 12:53:31 -0500

    Removed unused code, fixed bug with clearing inputs

--- packages/services/src/redux/sagas/settingsCategories.js
+++ packages/services/src/redux/sagas/settingsCategories.js
@@ -18,41 +18,6 @@ export function* fetchCategoriesSaga(action) {
   }
 }
 
-export function* updateCategorySaga(action) {
-  const data = {
-    name: action.payload.name,
-    slug: action.payload.slug,
-    attributesMap: {
-      'Sort Order': [action.payload.sort],
-      Parent: [action.payload.parent],
-    },
-  };
-
-  const response = yield call(updateCategory, {
-    method: 'put',
-    url: `${bundle.apiLocation()}/kapps/services/categories/${
-      action.payload.slug
-    }`,
-    data: JSON.stringify(data),
-  });
-
-  if (response.category) {
-  } else {
-  }
-
-  let promise = axios.request({
-    method: 'put',
-    url: `${bundle.apiLocation()}/kapps/services/categories/${
-      action.payload.slug
-    }`,
-    data: JSON.stringify(data),
-  });
-  promise = promise.then(response => ({ category: response.data.category }));
-  promise = promise.catch(response => ({ error: response }));
-  return promise;
-}
-
 export function* watchSettingsCategories() {
-  yield takeEvery(types.UPDATE_CATEGORY, updateCategorySaga);
   yield takeEvery(types.FETCH_CATEGORIES, fetchCategoriesSaga);
 }
