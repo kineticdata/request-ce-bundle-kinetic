aa602aa2786945e55e569e1fd9163fd7840bbbab
Chad Rehm <chad.rehm@kineticdata.com>
2018-10-04 15:45:37 -0500

    KIN-1035 initial commit for adding settings to queue

--- packages/queue/src/redux/sagas.js
+++ packages/queue/src/redux/sagas.js
@@ -2,7 +2,17 @@ import { all } from 'redux-saga/effects';
 import { watchApp } from './sagas/queueApp';
 import { watchQueue } from './sagas/queue';
 import { watchErrors } from './sagas/errors';
+import { watchForms } from './sagas/forms';
+import { watchSettingsQueue } from './sagas/settingsQueue';
+import { watchSettingsForms } from './sagas/settingsForms';
 
 export default function* sagas() {
-  yield all([watchErrors(), watchApp(), watchQueue()]);
+  yield all([
+    watchErrors(),
+    watchApp(),
+    watchQueue(),
+    watchForms(),
+    watchSettingsQueue(),
+    watchSettingsForms(),
+  ]);
 }
