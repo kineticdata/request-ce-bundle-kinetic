2e7d434fde1926d68a5f53c952e2bcfad51f8fae
homejames203 <james.davies@kineticdata.com>
2018-10-10 10:57:18 -0400

    Updated KappLinks to take an optional Kapp Slug for usage outside of the current Kapp

--- packages/common/src/components/KappRedirect.js
+++ packages/common/src/components/KappRedirect.js
@@ -3,12 +3,26 @@ import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import { Redirect } from 'react-router-dom';
 
-export const KappRedirectComponent = ({ kappSlug, to, ...rest }) => (
-  <Redirect {...rest} to={kappSlug ? `/kapps/${kappSlug}${to}` : to} />
-);
+/**
+ * Builds a Redirect to a kapp given a relative path, or uses the current kapp.
+ * @constructor
+ * @param {string} kappSlug - Optional Kapp Slug can be provided if not redirecting to the current kapp.
+ * @param {string} to - The relative path to redirect to.
+ */
+export const KappRedirectComponent = ({
+  to,
+  kappSlug,
+  currentKappSlug,
+  ...rest
+}) => {
+  const destinationKapp = kappSlug ? kappSlug : currentKappSlug;
+  const destination = destinationKapp ? `/kapps/${destinationKapp}${to}` : to;
+
+  return <Redirect {...rest} to={destination} />;
+};
 
 export const mapStateToProps = state => ({
-  kappSlug: state.app.config.kappSlug,
+  currentKappSlug: state.app.config.kappSlug,
   // Need to map in the current pathname because the connect HOC will
   // not let the inner Redirect update if mapStateToProps doesn't return
   // a different value (its implicitly a pure component).
