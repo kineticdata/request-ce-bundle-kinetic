be1f8bebba722bc417582e9716f3f9d47ac80374
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-10-23 16:03:38 -0500

    updated system message rendering to render tokens separately, adds support for handling message update system message KD-869

--- packages/discussions/src/components/SystemMessagesGroup.js
+++ packages/discussions/src/components/SystemMessagesGroup.js
@@ -1,13 +1,78 @@
 import React from 'react';
 import moment from 'moment';
-import { produceContent } from './MessagesGroup';
+import { List, Range } from 'immutable';
+import { MessageActionsContext } from './Discussion';
+
+export const PlainMessageContent = ({ content }) => {
+  const plainContent = content
+    .map(token => {
+      switch (token.type) {
+        case 'team':
+          return token.value.name;
+        case 'text':
+          return token.value.trim();
+        case 'user':
+          return token.value.displayName;
+        default:
+          return `(${token.type.toUpperCase()})`;
+      }
+    })
+    .join(' ');
+  return plainContent.length > 50
+    ? plainContent.substring(0, 50) + '...'
+    : plainContent;
+};
+
+export const SystemMessageContent = ({ content }) =>
+  List(content)
+    .map((token, i) => {
+      switch (token.type) {
+        case 'message':
+          return (
+            <MessageActionsContext.Consumer key={i}>
+              {actions => (
+                <span
+                  className="message-token"
+                  onClick={() => actions.viewMessageVersions(token.value)}
+                >
+                  [{' '}
+                  <PlainMessageContent key={i} content={token.value.content} />{' '}
+                  ]
+                </span>
+              )}
+            </MessageActionsContext.Consumer>
+          );
+        case 'team':
+          return (
+            <span key={i} className="team-token">
+              {token.value.name}
+            </span>
+          );
+        case 'text':
+          return (
+            <span key={i} className="text-token">
+              {token.value.trim()}
+            </span>
+          );
+        case 'user':
+          return (
+            <span key={i} className="user-token">
+              {token.value.displayName}
+            </span>
+          );
+        default:
+          return <span key={i}>({token.type.toUpperCase()})</span>;
+      }
+    })
+    .zip(Range().map(i => <span key={`space-${i}`}>&nbsp;</span>))
+    .toArray();
 
 export const SystemMessagesGroup = props => (
   <div className="messages-group">
     <div className="system-message-list">
       {props.messages.map(message => (
         <div className="system-message" key={message.id}>
-          {produceContent(message)}
+          <SystemMessageContent content={message.content} />
           <span className="timestamp">
             {moment(message.createdAt).format('h:mma')}
           </span>
