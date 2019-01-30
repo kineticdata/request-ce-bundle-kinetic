50051646546e6860cbf979c89f92bcf996d4ec31
Chad Rehm <chad.rehm@kineticdata.com>
2019-01-30 14:17:28 -0600

    AA3C1C Added search result save functionality to the common package.

--- packages/services/src/components/search_results/CatalogSearchResultsContainer.js
+++ packages/services/src/components/search_results/CatalogSearchResultsContainer.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
+import { lifecycle, compose } from 'recompose';
 import { CatalogSearchResults } from './CatalogSearchResults';
 import { displayableFormPredicate } from '../../utils';
+import { Utils } from 'common';
 
 const matches = (form, term) =>
   form.name.toLowerCase().includes(term.toLowerCase()) ||
@@ -9,14 +11,38 @@ const matches = (form, term) =>
 
 const mapStateToProps = (state, props) => {
   const query = props.match.params.query || '';
+
   return {
     query,
     forms: state.services.forms.data
       .filter(displayableFormPredicate)
       .filter(form => matches(form, query)),
+    kappSlug: state.app.config.kappSlug,
+    searchResultsFormExists: state.services.search.searchResultsFormExists,
   };
 };
 
-export const CatalogSearchResultsContainer = connect(mapStateToProps)(
-  CatalogSearchResults,
+const enhance = compose(
+  connect(mapStateToProps),
+  lifecycle({
+    componentDidMount() {
+      if (this.props.searchResultsFormExists) {
+        Utils.recordSearch(
+          this.props.kappSlug,
+          this.props.query,
+          this.props.forms,
+        );
+      }
+    },
+    componentWillReceiveProps(nextProp) {
+      if (
+        nextProp.searchResultsFormExists &&
+        this.props.query !== nextProp.query
+      ) {
+        Utils.recordSearch(nextProp.kappSlug, nextProp.query, nextProp.forms);
+      }
+    },
+  }),
 );
+
+export const CatalogSearchResultsContainer = enhance(CatalogSearchResults);
