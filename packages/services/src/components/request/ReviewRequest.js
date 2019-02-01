c3953b7853416b7ab13709a985cf0ae9e14b3c58
Chad Rehm <chad.rehm@kineticdata.com>
2019-02-01 15:19:06 -0600

    8A9EBD Added multiple navigation controls
    Marcin, Norm and myself decided on previous next buttons and a dropdown
    to select specific pages.

--- packages/services/src/components/request/ReviewRequest.js
+++ packages/services/src/components/request/ReviewRequest.js
@@ -1,91 +1,192 @@
-import React, { Fragment } from 'react';
-import { withState, withHandlers, compose, setDisplayName } from 'recompose';
-import { I18n } from '../../../../app/src/I18nProvider';
+import React from 'react';
+import { withState, withHandlers, compose } from 'recompose';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 import { CoreForm } from 'react-kinetic-core';
+import classNames from 'classnames';
+import { I18n } from '../../../../app/src/I18nProvider';
 
 const globals = import('common/globals');
 
-export const ReviewRequestComponent = ({
-  handleLoaded,
+export const NavigationControlsComponent = ({
   handleNextPage,
   handlePreviousPage,
-  formLoaded,
+  dropdownOpen,
+  handleToggle,
+  displayPages,
+  handlePageChange,
+  currentLoc,
+}) => (
+  <div className="btn-group">
+    <button
+      type="button"
+      className="btn btn-inverse"
+      onClick={handlePreviousPage}
+      value={currentLoc}
+      disabled={currentLoc <= 0}
+    >
+      <span className="icon">
+        <span className="fa fa-fw fa-caret-left" />
+      </span>
+    </button>
+    <div className="navigation-dropdown">
+      <Dropdown isOpen={dropdownOpen} toggle={handleToggle}>
+        <DropdownToggle caret>{`Page ${currentLoc + 1} `}</DropdownToggle>
+        <DropdownMenu>
+          {displayPages.map((pageName, idx) => {
+            const str = `${idx + 1}. ${pageName}`;
+            return (
+              <DropdownItem
+                key={pageName}
+                value={idx}
+                onClick={handlePageChange}
+              >
+                {str.length > 13 ? `${str.slice(0, 13).trim()}...` : str}
+              </DropdownItem>
+            );
+          })}
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+    <button
+      type="button"
+      className="btn btn-inverse"
+      onClick={handleNextPage}
+      value={currentLoc}
+      disabled={currentLoc >= displayPages.length - 1}
+    >
+      <span className="icon">
+        <span className="fa fa-fw fa-caret-right" />
+      </span>
+    </button>
+  </div>
+);
+
+const setState = (props, loc) => {
+  props.setReviewPage(props.displayPages[loc]);
+  props.setCurrentLoc(loc);
+  props.setFormLoaded(false);
+};
+
+const handlePreviousPage = props => event => {
+  const loc = parseInt(event.currentTarget.value, 10) - 1;
+  setState(props, loc);
+};
+
+const handleNextPage = props => event => {
+  const loc = parseInt(event.currentTarget.value, 10) + 1;
+  setState(props, loc);
+};
+
+const handlePageChange = props => event => {
+  const loc = parseInt(event.currentTarget.value, 10);
+  setState(props, loc);
+};
+
+const handleToggle = props => () => {
+  props.setDropdownOpen(!props.dropdownOpen);
+};
+
+const NavigationControls = compose(
+  withState('dropdownOpen', 'setDropdownOpen', false),
+  withHandlers({
+    handlePreviousPage,
+    handleNextPage,
+    handleToggle,
+    handlePageChange,
+  }),
+)(NavigationControlsComponent);
+
+export const ReviewRequestComponent = ({
+  handleLoaded,
+  reviewPage,
   kappSlug,
   submission,
-  disabledNextPage,
-  disabledPreviousPage,
-  reviewPage,
+  formLoaded,
+  displayTop,
+  multiPageForm,
+  displayPages,
+  currentLoc,
+  ...restProps
 }) => {
-  return (
-    <Fragment>
-      <I18n context={`kapps.${kappSlug}.forms.${submission.form.slug}`}>
-        <CoreForm
-          loaded={handleLoaded}
-          submission={submission.id}
-          review={reviewPage}
-          globals={globals}
-        />
-      </I18n>
-      {formLoaded && (
-        <Fragment>
-          <button onClick={handlePreviousPage} disabled={disabledPreviousPage}>
-            Previous
-          </button>
-          <button onClick={handleNextPage} disabled={disabledNextPage}>
-            Next
-          </button>
-        </Fragment>
+  return multiPageForm ? (
+    <div className="multi-page--service">
+      {displayTop && (
+        <div className="page-display--top">
+          <h3>{displayPages[currentLoc]}</h3>
+          <NavigationControls {...{ ...restProps, currentLoc, displayPages }} />
+        </div>
       )}
-    </Fragment>
+      <div className="form-wrapper--review">
+        <div
+          className={classNames({
+            hidden: !formLoaded,
+          })}
+        >
+          <I18n context={`kapps.${kappSlug}.forms.${submission.form.slug}`}>
+            <CoreForm
+              loaded={handleLoaded}
+              submission={submission.id}
+              review={reviewPage}
+              globals={globals}
+            />
+          </I18n>
+        </div>
+        <div
+          className={classNames('load-wrapper', {
+            hidden: formLoaded,
+          })}
+        >
+          <span className="fa fa-spinner fa-spin fa-lg fa-fw" />
+        </div>
+      </div>
+      {formLoaded && (
+        <div className="page-display--bottom">
+          <NavigationControls {...{ ...restProps, currentLoc, displayPages }} />
+        </div>
+      )}
+    </div>
+  ) : (
+    <I18n context={`kapps.${kappSlug}.forms.${submission.form.slug}`}>
+      <CoreForm
+        loaded={handleLoaded}
+        submission={submission.id}
+        review
+        globals={globals}
+      />
+    </I18n>
   );
 };
 
 export const handleLoaded = props => form => {
-  props.setDisplayPages(form.displayablePages());
-  props.setCurrentPage(form.page().name());
+  // if form has already been loaded don't set these states again
+  if (!props.displayPages) {
+    props.setDisplayPages(form.displayablePages());
+    props.setDisplayTop(true);
+    if (form.displayablePages().length > 1) {
+      props.setMultiPageForm(true);
+    }
+    const currentLoc = form
+      .displayablePages()
+      .findIndex(currentPage => currentPage === form.page().name());
+    props.setCurrentLoc(currentLoc);
+  }
+
   props.setFormLoaded(true);
-
-  const currentLoc = form
-    .displayablePages()
-    .findIndex(currentPage => currentPage === form.page().name());
-
-  if (currentLoc <= 0) {
-    props.disablePrevious(true);
-  } else {
-    props.disablePrevious(false);
-  }
-
-  if (currentLoc >= form.displayablePages().length - 1) {
-    props.disableNext(true);
-  } else {
-    props.disableNext(false);
-  }
-};
-
-const handlePreviousPage = props => event => {
-  const currentLoc = props.displayPages.findIndex(
-    currentPage => currentPage === props.currentPage,
-  );
-  props.setReviewPage(props.displayPages[currentLoc - 1]);
-  props.setFormLoaded(false);
-};
-
-const handleNextPage = props => event => {
-  const currentLoc = props.displayPages.findIndex(
-    currentPage => currentPage === props.currentPage,
-  );
-  props.setReviewPage(props.displayPages[currentLoc + 1]);
-  props.setFormLoaded(false);
 };
 
 const enhance = compose(
   withState('formLoaded', 'setFormLoaded', false),
+  withState('displayTop', 'setDisplayTop', false),
   withState('displayPages', 'setDisplayPages', null),
-  withState('currentPage', 'setCurrentPage', null),
-  withState('disabledNextPage', 'disableNext', true),
-  withState('disabledPreviousPage', 'disablePrevious', true),
+  withState('multiPageForm', 'setMultiPageForm', false),
+  withState('currentLoc', 'setCurrentLoc', 0),
   withState('reviewPage', 'setReviewPage', true),
-  withHandlers({ handleLoaded, handlePreviousPage, handleNextPage }),
+  withHandlers({ handleLoaded }),
 );
 
 export const ReviewRequest = enhance(ReviewRequestComponent);
