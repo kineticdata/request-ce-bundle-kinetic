4a6645de4ae6e8c5ed3b6dbbc75dfaeb4ab71428
Norman Orstad <norm.orstad@kineticdata.com>
2019-03-13 16:32:20 -0500

    Second draft of design system cards

--- packages/design-system/src/CardsPage.js
+++ packages/design-system/src/CardsPage.js
@@ -79,16 +79,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -113,16 +113,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -147,16 +147,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -181,16 +181,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -215,16 +215,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -249,16 +249,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -285,7 +285,38 @@ export const CardsPage = () => (
             bay that lies between the mouths of the Po and the Piave rivers.
           </p>
           <div className="card__footer">
-            <button className="btn btn-link text-sm-left">View place→</button>
+            <button className="btn btn-link text-sm-left pl-0">
+              View place→
+            </button>
+          </div>
+        </div>
+        <hr />
+
+        <div className="card p-4">
+          <div className="card__header d-flex flex-row-reverse">
+            <div
+              title="Woman holding a mug"
+              className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+              style={{
+                backgroundImage:
+                  "url('https://tailwindcss.com/img/card-left.jpg')",
+              }}
+            />
+            <span className="tag tag--active tag--small">Tag Small</span>
+          </div>
+          <span className="card__date">November 2018</span>
+          <h1 className="card__title">Venice Islands</h1>
+          <p>
+            Venice is a city in northeastern Italy and the capital of the Veneto
+            region. It is situated across a group of 118 small islands that are
+            separated by canals and linked by bridges, of which there are 400.
+            The islands are located in the shallow Venetian Lagoon, an enclosed
+            bay that lies between the mouths of the Po and the Piave rivers.
+          </p>
+          <div className="card__footer">
+            <button className="btn btn-link text-sm-left pl-0">
+              View place→
+            </button>
           </div>
         </div>
         <hr />
@@ -294,25 +325,165 @@ export const CardsPage = () => (
           <p>This is some text within a card</p>
         </div>
         <hr />
+        <h2>Call to Action</h2>
+        <div className="cards__wrapper">
+          <div className="card card--cta card--blue" style={{ width: '24%' }}>
+            <i className="fa fa-2x fa-photo" />
+            Pictures
+          </div>
+          <div className="card card--cta card--green" style={{ width: '24%' }}>
+            <i className="fa fa-2x fa-life-ring" />
+            FAQ
+          </div>
+          <div
+            className="card card--cta card--green text-center"
+            style={{ width: '40%' }}
+          >
+            <h4>November 2019</h4>
+            <h1>Euro Trip</h1>
+            <span className="card--cta__rule" />
+          </div>
+        </div>
+        {/* -- Profile Card -- */}
+        <hr />
+        <h2>Profile</h2>
+        <div className="card card--profile">
+          <div className="card__header">
+            <img
+              className="sb-avatar__image"
+              width={96}
+              height={96}
+              src={'https://placekitten.com/192'}
+              alt={'norm.orstad@kineticdata.com'}
+              style={{
+                maxWidth: '100%',
+                width: '96px',
+                height: '96px',
+                borderRadius: '100%',
+              }}
+            />
+          </div>
+          <div className="card__body">
+            <h2>Austin Gomez</h2>
+            <p>
+              <a href="mailto:">austin@gomez.org</a>
+            </p>
+            <p>555-555-1234</p>
+            <a href="" className="btn btn-secondary">
+              View Profile
+            </a>
+          </div>
+        </div>
+        {/* -- Team Card -- */}
+        <hr />
+        <h2>Teams</h2>
+        <div className="card card--team">
+          <div
+            className="card__header card__header--team"
+            style={{ backgroundColor: 'rgb(166, 48, 150)' }}
+          >
+            <span />
+            <i className="fa fa-users card-icon" />
+            <span />
+          </div>
+          <div className="card__body">
+            <h1>Team Name</h1>
+            <p>Description of team and what it is the team does</p>
+          </div>
+          <div className="card__footer">
+            <h3>Members</h3>
+            <div className="card-members__wrapper">
+              <span>
+                <div
+                  className="sb-avatar sb-avatar--gravatar"
+                  style={{
+                    display: 'inline-block',
+                    verticalAlign: 'middle',
+                    width: '28px',
+                    height: '28px',
+                    borderRadius: '100%',
+                  }}
+                >
+                  <img
+                    className="sb-avatar__image"
+                    width={28}
+                    height={28}
+                    src={'https://placekitten.com/150'}
+                    alt={'Kitten Name'}
+                    style={{
+                      maxWidth: '100%',
+                      width: '28px',
+                      height: '28px',
+                      borderRadius: '100%',
+                    }}
+                  />
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <h2>Meta Information</h2>
         <div className="card">
           <p>This is some text within a card</p>
           <p>A second row of text within a card</p>
           <span className="meta">
             <dl className="row">
               <div className="col">
-                <dt>Test</dt>
-                <dd>Meta</dd>
+                <dt className="">Confirmation</dt>
+                <dd className="">7CA127</dd>
+              </div>
+              <div className="col">
+                <dt className="">Submitted</dt>
+                <dd className="">
+                  <span className="time-ago">
+                    <span className="time-ago__text">16 days ago</span>
+                  </span>
+                </dd>
+              </div>
+              <div className="col">
+                <dt className="">Est. Completion</dt>
+                <dd className="">
+                  <span className="time-ago">
+                    <span className="time-ago__text">9 days ago</span>
+                  </span>
+                </dd>
               </div>
             </dl>
           </span>
         </div>
+        {/* -- Appointments -- */}
         <hr />
-        <h2>Example Advanced Card</h2>
-        <div className="card card--request" style={{ width: '50%' }}>
+        <h2>Appointments</h2>
+        <div className="card card--appt">
+          <i
+            className="fa fa-calendar fa-fw card-icon"
+            style={{ background: 'rgb(255, 74, 94)' }}
+          />
+          <div className="card__body">
+            <h1 className="card__title">
+              Thursday, January 31, 2019
+              <span className="submission-status submission-status--complete">
+                Completed
+              </span>
+            </h1>
+            <p className="card__subtitle">1:00 PM - 1:30 PM</p>
+            <p className="card__text">Testing cancel button</p>
+            <a className="btn btn-link text-left pl-0" href="/">
+              View Details →
+            </a>
+          </div>
+        </div>
+        <hr />
+        {/* -- Advanced Cards -- */}
+        <h2>Advanced Card</h2>
+        <div className="card card--request">
           <h1 className="card__title">
             <i className="fa fa-file-text-o fa-fw card-icon" />
             <span>Card Title</span>
-            <span className="status status--green">Submitted</span>
+            <span className="status submission-status submission-status--open">
+              Submitted
+            </span>
             <div className="dropdown">
               <button
                 type="button"
@@ -320,16 +491,16 @@ export const CardsPage = () => (
                 aria-expanded="false"
                 className="btn-sm btn btn-icon"
               >
-                <span class="fa fa-ellipsis-h fa-2x" />
+                <span className="fa fa-ellipsis-h fa-2x" />
               </button>
               <div
-                tabindex="-1"
+                tabIndex="-1"
                 role="menu"
                 aria-hidden="true"
                 className="dropdown-menu dropdown-menu-right"
               >
                 <a
-                  tabindex="0"
+                  tabIndex="0"
                   className="dropdown-item"
                   href="#/kapps/services"
                 >
@@ -338,86 +509,31 @@ export const CardsPage = () => (
               </div>
             </div>
           </h1>
-          <p>Basic description</p>
+          <p className="card__text">Basic description</p>
           <span className="meta">
             <dl className="row">
               <div className="col">
                 <dt className="">Confirmation</dt>
-                <dd class="">7CA127</dd>
+                <dd className="">7CA127</dd>
               </div>
               <div className="col">
                 <dt className="">Submitted</dt>
                 <dd className="">
-                  <span class="time-ago">
-                    <span
-                      className="time-ago__text"
-                      id="tooltip-4fa3c14f-7ac9-42a0-9703-086eb961a37d"
-                    >
-                      16 days ago
-                    </span>
+                  <span className="time-ago">
+                    <span className="time-ago__text">16 days ago</span>
                   </span>
                 </dd>
               </div>
               <div className="col">
-                <dt class="">Est. Completion</dt>
+                <dt className="">Est. Completion</dt>
                 <dd className="">
                   <span className="time-ago">
-                    <span
-                      className="time-ago__text"
-                      id="tooltip-e13fa43d-c9dc-4627-bc1a-d8ef87fdbd23"
-                    >
-                      9 days ago
-                    </span>
+                    <span className="time-ago__text">9 days ago</span>
                   </span>
                 </dd>
               </div>
             </dl>
           </span>
-        </div>
-        <div className="flex-row">
-          <div className="highlight">
-            <pre>{`<div className="card card--request">
-    <h1>
-      <i className="fa fa-file-text-o fa-fw card-icon" />
-      <span>Card Title</span>
-      <span className="status status--green">Submitted</span>
-      <div className="dropdown">
-        <button type="button" aria-haspopup="true" aria-expanded="false" className="btn-sm btn btn-icon">
-          <span class="fa fa-ellipsis-h fa-2x"></span>
-        </button>
-        <div tabindex="-1" role="menu" aria-hidden="true" className="dropdown-menu dropdown-menu-right">
-          <a tabindex="0" className="dropdown-item" href="#/kapps/services">Request to Cancel</a>
-        </div>
-      </div>
-    </h1>
-    <p>Basic description</p>
-    <span className="meta">
-      <dl className="row">
-        <div className="col">
-          <dt className="">Confirmation</dt>
-          <dd class="">7CA127</dd>
-        </div>
-        <div className="col">
-          <dt className="">Submitted</dt>
-          <dd className="">
-            <span class="time-ago">
-              <span className="time-ago__text" id="tooltip-4fa3c14f-7ac9-42a0-9703-086eb961a37d">16 days ago</span>
-            </span>
-          </dd>
-        </div>
-        <div className="col">
-          <dt class="">Est. Completion</dt>
-          <dd className="">
-            <span className="time-ago">
-              <span className="time-ago__text" id="tooltip-e13fa43d-c9dc-4627-bc1a-d8ef87fdbd23">9 days ago</span>
-            </span>
-          </dd>
-        </div>
-      </dl>
-    </span>
-  </div>
-`}</pre>
-          </div>
         </div>
       </div>
     </div>
