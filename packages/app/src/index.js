d4d79baf4fc76d049e002ecaad728b7fe517f925
Norman Orstad <norm.orstad@kineticdata.com>
2020-04-23 08:37:08 -0500

    KP-795. Remove meta "maximum scale" that prevent people with low vision concerns

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -23,10 +23,7 @@ const ConnectedKineticLib = connect(state => ({
 ReactDOM.render(
   <Fragment>
     <Helmet>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Helmet>
     <Provider store={store}>
       <ConnectedKineticLib
