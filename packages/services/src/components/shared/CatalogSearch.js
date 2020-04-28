dfd9d42bbb4f3a12937b2910f4d90e7d6defd103
Norman Orstad <norm.orstad@kineticdata.com>
2020-04-28 15:39:48 -0500

    KP-795. Services search box with better markup for #a11y

--- packages/services/src/components/shared/CatalogSearch.js
+++ packages/services/src/components/shared/CatalogSearch.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { I18n } from '@kineticdata/react';
 
 export const CatalogSearch = props => (
-  <form onSubmit={props.submitHandler(props)} className="search-box__form">
+  <form
+    role="search"
+    onSubmit={props.submitHandler(props)}
+    className="search-box__form"
+  >
     <I18n
       render={translate => (
         <input
-          type="text"
+          type="search"
           placeholder={translate('Search services...')}
           value={props.searchTerm}
           autoFocus
-          aria-label="Search Services"
+          aria-label="Search Services..."
           onChange={event => props.catalogSearchInput(event.target.value)}
         />
       )}
     />
-    <button type="submit" aria-label="Search Button">
-      <span className="fa fa-fw fa-search" />
+    <button type="submit" aria-label="Submit Search">
+      <span className="fa fa-fw fa-search" focusable="false" />
     </button>
   </form>
 );
