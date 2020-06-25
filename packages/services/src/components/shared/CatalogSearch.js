572f0ccbadb94e51c8e31cc15d6254012ddf4846
Norman Orstad <norm.orstad@kineticdata.com>
2020-06-25 11:44:39 -0500

    KP-1467. More :focus fixes; Cleaned up search boxes :focus;

--- packages/services/src/components/shared/CatalogSearch.js
+++ packages/services/src/components/shared/CatalogSearch.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { I18n } from '@kineticdata/react';
 
 export const CatalogSearch = props => (
-  <form
-    role="search"
-    onSubmit={props.submitHandler(props)}
-    className="search-box__form"
-  >
-    <I18n
-      render={translate => (
-        <input
-          type="search"
-          placeholder={translate('Search services...')}
-          value={props.searchTerm}
-          autoFocus
-          aria-label="Search Services..."
-          onChange={event => props.catalogSearchInput(event.target.value)}
-        />
-      )}
-    />
-    <button type="submit" aria-label="Submit Search">
-      <span className="fa fa-fw fa-search" focusable="false" />
-    </button>
+  <form onSubmit={props.submitHandler(props)} className="search-box__form">
+    <div role="search">
+      <I18n
+        render={translate => (
+          <input
+            type="search"
+            placeholder={translate('Search services...')}
+            value={props.searchTerm}
+            autoFocus
+            role="searchbox"
+            aria-label="Search Services..."
+            onChange={event => props.catalogSearchInput(event.target.value)}
+          />
+        )}
+      />
+      <button type="submit" aria-label="Submit Search">
+        <span className="fa fa-fw fa-search" focusable="false" />
+      </button>
+    </div>
   </form>
 );
